ee9bd52b004de5db48a9828bbde0c0b30e867561
acuon <83219534+acuon@users.noreply.github.com>
2021-04-30 10:27:18 +0530

    Create BirthdayCakeCandles.java

---
+++ Java/Problem Solving/BirthdayCakeCandles.java
@@ -0,0 +1,52 @@
+//https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+    Collections.sort(candles);
+    int n = candles.size();
+    int max = candles.get(n-1);
+    int count = 0;
+    for ( int i=0; i<n; i++) {
+        if (max == candles.get(i)) {
+            count = count + 1;
+        }
+    }
+    return count;
+   
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
