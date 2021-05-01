928ad0e7a66c5933b56cdb8e1a9713b9669d28e3
acuon <83219534+acuon@users.noreply.github.com>
2021-05-02 02:45:15 +0530

    Create JavaStringTokens.java

---
+++ Java/JavaStringTokens.java
@@ -0,0 +1,70 @@
+/*
+Given a string, s, matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens. We define a token to be one or more consecutive English alphabetic letters. Then, print the number of tokens, followed by each token on a new line.
+
+
+Input Format
+
+A single string, s.
+
+Constraints
+
+0 < length of s < 400000
+s is composed of any of the following: English alphabetic letters, blank spaces, exclamation points (!), commas (,),
+question marks (?), periods (.), underscores (_), apostrophes ('), and at symbols (@).
+
+Output Format
+
+On the first line, print an integer, n, denoting the number of tokens in string s (they do not need to be unique).
+Next, print each of the n tokens on a new line in the same order as they appear in input string s.
+
+Sample Input
+
+He is a very very good boy, isn't he?
+
+Sample Output
+
+10
+He
+is
+a
+very
+very
+good
+boy
+isn
+t
+he
+
+Explanation
+
+We consider a token to be a contiguous segment of alphabetic characters. There are a total of 10 such tokens in string s,
+and each token is printed in the same order in which it appears in string s.
+*/
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        //System.out.println(s);
+        //String regex = "[ !,?._'@]+";
+        s=s.trim(); //removes all the white spaces
+        if(s.length()>0 && s.length()<=400000) { //contraints
+            String[] s1 = s.split("[ !,?._'@]+");
+            System.out.println(s1.length);
+            for (String a : s1) {
+                System.out.println(a);
+            }
+        }
+        else {
+            System.out.println(0);
+        }
+        scan.close();
+    }
+}
+
