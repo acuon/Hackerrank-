39f3cfd2ef1bde57fa5a4c7bc8ad51ed7d499c3e
acuon <83219534+acuon@users.noreply.github.com>
2021-05-01 15:37:47 +0530

    Day 01 | Data Types

---
+++ 30DaysChallenge/DataTypes.java
@@ -0,0 +1,25 @@
+//https://www.hackerrank.com/challenges/30-data-types/problem
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        int i2 = scan.nextInt();
+        double d2 = scan.nextDouble();
+        scan.nextLine();
+        String s2= scan.nextLine();
+        System.out.println(i+i2);
+        System.out.println(d+d2);
+        System.out.println(s+s2);
+
+        scan.close();
