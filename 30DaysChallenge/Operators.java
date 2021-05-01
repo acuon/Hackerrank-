c6c402bbadb307441a2ffe5b8e4f72385dcf6ec0
acuon <83219534+acuon@users.noreply.github.com>
2021-05-01 15:50:26 +0530

    Day 02 | Operators

---
+++ 30DaysChallenge/Operators.java
@@ -0,0 +1,37 @@
+//https://www.hackerrank.com/challenges/30-operators/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+    double tip = ((tip_percent * meal_cost)/100);
+    double tax = ((tax_percent * meal_cost)/100);
+    double total_cost = meal_cost + tip + tax;
+    System.out.println(Math.round(total_cost));
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+}
