8d0007e5bedb76e5c7d4917c0ae50890bcf52156
acuon <83219534+acuon@users.noreply.github.com>
2021-05-05 19:10:31 +0530

    Day 4 | Geometric Distribution 1

--- 10DaysofStatistics/GeometricDistribution1.java
+++ 10DaysofStatistics/GeometricDistribution1.java
@@ -4,8 +4,15 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
+    
     public static void main(String[] args) {
+    /*
+    Scanner sc = new Scanner(System.in);
+    int numerator = sc.nextInt();
+    int denominator = sc.nextInt();
+    double p = (double)numerator/denominator;
+    int n = sc.nextInt();
+    */
     double p = 1d/3d; 
     //double p = (double)1/3; 
     //double p = 0.3333333333; //p = 1/3 = 0.333333333
