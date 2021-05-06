5712ab7ef51b85e61bf26576ed0789130e48e211
acuon <83219534+acuon@users.noreply.github.com>
2021-05-06 18:51:39 +0530

    Day 5 | Poisson Distribution 1

---
+++ 10DaysofStatistics/PoissonDistribution.java
@@ -0,0 +1,27 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    double lambda = 2.5;
+    int k = 5;
+    double result = 0;
+    result = poisson(k,lambda);
+    System.out.format("%.3f",result);
+    
+    }
+    private static double poisson(int k, double lambda) {
+        return (Math.pow(lambda,k)*Math.exp(-1*lambda))/factorial(k);
+    }
+    private static long factorial(int k) {
+        long fact = 1;
+        while(k>0) {
+            fact = fact*k;
+            k--;
+        }
+        return fact;
+    }
+}
