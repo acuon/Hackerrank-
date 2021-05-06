9979d80f47e25be26353655844999f417c588770
acuon <83219534+acuon@users.noreply.github.com>
2021-05-06 14:29:37 +0530

    Day 07 | Arrays

---
+++ 30DaysChallenge/Arrays.java
@@ -0,0 +1,34 @@
+//https://www.hackerrank.com/challenges/30-arrays/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+        
+        //System.out.println(n);
+        for (int i = n-1; i>=0; i--) {
+            System.out.printf(arr.get(i)+" ");
+        }
+
+        bufferedReader.close();
+    }
+}
