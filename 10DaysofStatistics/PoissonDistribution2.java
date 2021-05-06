ea644f3e01b24389b947a4c8d787eab118035955
acuon <83219534+acuon@users.noreply.github.com>
2021-05-06 18:54:29 +0530

    Day 5 | Poisson Distribution 2

---
+++ 10DaysofStatistics/PoissonDistribution2.java
@@ -0,0 +1,25 @@
+//https://www.hackerrank.com/challenges/s10-poisson-distribution-2/problem
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    /*
+    Scanner sc = new Scanner(System.in);
+    double A = sc.nextDouoble();
+    double B = sc.nextDouble();
+    
+    */
+    double dailyCostA = 0;
+    double dailyCostB = 0;
+    double lambdaA = 0.88;
+    double lambdaB = 1.55;
+    dailyCostA = 160 + 40*(lambdaA + (lambdaA * lambdaA));
+    dailyCostB = 128 + 40*(lambdaB + (lambdaB * lambdaB));
+    System.out.format("%.3f%n",dailyCostA);
+    System.out.format("%.3f",dailyCostB);
+    
+    }
+}
