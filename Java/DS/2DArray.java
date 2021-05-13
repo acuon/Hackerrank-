c84aee1747509e7e92a33af771365603a64fe3ce
acuon <83219534+acuon@users.noreply.github.com>
2021-05-13 19:57:20 +0530

    Create 2DArray.java

---
+++ Java/DS/2DArray.java
@@ -0,0 +1,51 @@
+//https://www.hackerrank.com/challenges/java-2d-array/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+        
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        int sum = 0;
+        int max = -10000;
+        for(int i=0; i<arr.size()-2; i++) {
+            for(int j=0; j<arr.size()-2; j++) {
+                sum = arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2) + arr.get(i+1).get(j+1) + arr.get(i+2).get(j)  + arr.get(i+2).get(j+1) + arr.get(i+2).get(j+2);
+                //System.out.println(sum);
+                if(sum>max) {
+                    max = sum;
+                }
+            }
+        }
+        //System.out.println(sum);
+        System.out.println(max);
+
+        bufferedReader.close();
+    }
+}
