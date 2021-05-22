b45e6545e64341f377b53b6bc4c8ffd7948eef62
acuon <83219534+acuon@users.noreply.github.com>
2021-05-22 19:41:26 +0530

    Update JavaSubarray.java

--- Java/JavaSubarray.java
+++ Java/JavaSubarray.java
@@ -32,5 +32,34 @@ public class Solution {
         }
         System.out.println(negative_sums);
     }
-    
 }
+
+
+
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();   
+        }
+        sc.close();
+        int negative = 0;
+        for(int i=0; i<arr.length; i++) {
+            int sum = 0;
+            for(int j=i; j<arr.length; j++) {
+                sum = sum + arr[j];
+                //System.out.println(sum);
+                if(sum<0) {
+                    negative++;
+                }
+            }
+        }
+        System.out.println(negative);
+    }   
+}
+
