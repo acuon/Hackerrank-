c7f0fc22274cd57505fbf8d9c034017b12f406d9
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-05-31 11:14:48 +0530

    ^ for start

--- Regex/MatchingCharacterRanges.java
+++ Regex/MatchingCharacterRanges.java
@@ -1,6 +1,17 @@
 /*
 https://www.hackerrank.com/challenges/matching-range-of-characters/problem
+
+Write a RegEx that will match a string satisfying the following conditions:
+
+    The string's length is >=5.
+    The first character must be a lowercase English alphabetic character.
+    The second character must be a positive digit. Note that we consider zero to be neither positive nor negative.
+    The third character must not be a lowercase English alphabetic character.
+    The fourth character must not be an uppercase English alphabetic character.
+    The fifth character must be an uppercase English alphabetic character.
+    
 */
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
