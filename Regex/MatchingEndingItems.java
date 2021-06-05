f7f21e850e8594d1cd328cc8f17802d11ac25f19
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-05 17:28:30 +0530

    $ --> for end

--- Regex/MatchingEndingItems.java
+++ Regex/MatchingEndingItems.java
@@ -2,7 +2,6 @@
 //https://www.hackerrank.com/challenges/matching-ending-items/problem
 
 Write a RegEx to match a test string, S, under the following conditions:
-
   -S should consist of only lowercase and uppercase letters (no numbers or symbols).
   -S should end in s.
 */
