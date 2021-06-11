5d90a8dc5c7a989f5ae41fa8219898f87f38c2bb
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-11 11:00:42 +0530

    Update MinimumHeightTriangle.java
            a = 100
            b = 17
            double h = 2*(double)area/(double)trianglebase --> will give 11.7647058824 ---> after rounding --> 12.0
            double h = 2*area/trianglebase --> will give 11.0 ---> after rounding --> 11.0

--- Mathematics/Fundamentals/MinimumHeightTriangle.java
+++ Mathematics/Fundamentals/MinimumHeightTriangle.java
@@ -29,7 +29,6 @@ class Result {
     }
 
 }
-
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
