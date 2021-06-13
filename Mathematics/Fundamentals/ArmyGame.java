a2ee7d796c52f41de898a053678c91f7a40f5e4f
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-13 21:53:15 +0530

    Create ArmyGame.java

---
+++ Mathematics/Fundamentals/ArmyGame.java
@@ -0,0 +1,46 @@
+//https://www.hackerrank.com/challenges/game-with-cells/problem
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int gameWithCells(int n, int m) {
+        return ((n+n%2)*(m+m%2))/4;
+      /*
+      we can supply four army bases(boxes) using 1 supply box if we drop it at the intersection of the four bases(boxes)
+    
+      */
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.gameWithCells(n, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
