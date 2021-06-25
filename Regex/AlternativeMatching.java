d27bfc6b88372f49422beb35c066949243afcfe1
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-06-25 16:51:16 +0530

    using | (vertical bar)

--- Regex/AlternativeMatching.java
+++ Regex/AlternativeMatching.java
@@ -2,6 +2,7 @@
 //https://www.hackerrank.com/challenges/alternative-matching/problem
 
 
+
 Alternations, denoted by the | character, match a single item out of several possible items separated by the vertical bar.
 When used inside a character class, it will match characters; when used inside a group, it will match entire expressions
 (i.e., everything to the left or everything to the right of the vertical bar). We must use parentheses to limit the use of alternations.
