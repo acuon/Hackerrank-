61ea46f73451c814318dfc5cd242018cab8a312f
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-07-06 23:35:03 +0530

    Update TwoDArray.java

--- Interview_Preparation/TwoDArray.java
+++ Interview_Preparation/TwoDArray.java
@@ -16,7 +16,7 @@ class Result {
 
     /*
      * Complete the 'hourglassSum' function below.
-     *
+     
      * The function is expected to return an INTEGER.
      * The function accepts 2D_INTEGER_ARRAY arr as parameter.
      */
