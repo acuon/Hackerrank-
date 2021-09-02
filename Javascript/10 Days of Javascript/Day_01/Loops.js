118c72cd2f641830f2baa7ee8937fba25128fe65
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-09-02 12:50:48 +0530

    Update Loops.js

--- Javascript/10 Days of Javascript/Day_01/Loops.js
+++ Javascript/10 Days of Javascript/Day_01/Loops.js
@@ -30,6 +30,8 @@ function readLine() {
  * Complete the vowelsAndConsonants function.
  * Print your output using 'console.log()'.
  */
+
+//function 1
 function vowelsAndConsonants(s) {
     var str = "";
     for(var i=0; i<s.length; i++) {
@@ -41,6 +43,25 @@ function vowelsAndConsonants(s) {
     }
     console.log(str);
 }
+
+//function 2
+function vowelsAndConsonants(s) {
+    const vowels = 'aeiou';
+    var consonants = '';
+    
+    for(var i = 0; i < s.length; i++) {
+       if (vowels.includes(s[i])) {
+           console.log(s[i]);
+       }
+       else {
+           consonants += s[i] + '\n';
+       }
+    }
+    
+    console.log(consonants.trim());
+}
+
+
 
 
 function main() {
