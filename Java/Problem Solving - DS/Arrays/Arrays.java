a7f11b4e00a0edfc92fffab660348dca35806f2c
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-09-06 13:11:34 +0530

    Create Arrays.java

---
+++ Java/Problem Solving - DS/Arrays/Arrays.java
@@ -0,0 +1,57 @@
+/*
+https://www.hackerrank.com/challenges/arrays-ds/copy-from/212044996
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> reverseArray(List<Integer> a) {
+    //Scanner sc = new Scanner(System.in);
+    //System.out.println(a);
+    int n = a.size();
+    //System.out.println(n);
+    ArrayList<Integer> reverse = new ArrayList<Integer>();
+    for (int i=n-1; i>=0; i--) {
+        //System.out.printf(a.get(i)+" ");
+        reverse.add(a.get(i));
+    }
+    return reverse;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> res = Result.reverseArray(arr);
+
+        bufferedWriter.write(
+            res.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
