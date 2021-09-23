9627622d974d92d284be1a4e18caa5bd2bd3e9a3
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-09-24 00:13:46 +0530

    Update TimeConversion.java

--- Problem Solving/Algorithms/Warmup/TimeConversion.java
+++ Problem Solving/Algorithms/Warmup/TimeConversion.java
@@ -35,6 +35,7 @@ class Result {
 
 }
 
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
