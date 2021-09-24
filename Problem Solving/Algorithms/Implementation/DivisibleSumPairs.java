7bbb7f18f2d41c14a27df8c566c23a244182e3f8
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-09-24 16:19:44 +0530

    Update DivisibleSumPairs.java

--- Problem Solving/Algorithms/Implementation/DivisibleSumPairs.java
+++ Problem Solving/Algorithms/Implementation/DivisibleSumPairs.java
@@ -14,16 +14,6 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'divisibleSumPairs' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. INTEGER n
-     *  2. INTEGER k
-     *  3. INTEGER_ARRAY ar
-     */
-
     public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
     int count = 0;
     for(int i=0; i<ar.size(); i++) {
