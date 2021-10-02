62ed103f8bc6b9f838086e6003a09875847e3205
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-10-02 21:54:58 +0530

    Update JavaAnagrams.java

--- Java/JavaAnagrams.java
+++ Java/JavaAnagrams.java
@@ -2,17 +2,15 @@
 Two strings, a and b, are called anagrams if they contain all the same characters in the same frequencies.
 For this challenge, the test is not case-sensitive. For example, the anagrams of CAT are CAT, ACT, tac, TCA, aTC, and CtA.
 
+
 Function Description
 
 Complete the isAnagram function in the editor.
-
 isAnagram has the following parameters:
-
     string a: the first string
     string b: the second string
 
 Returns
-
 boolean: If a and b are case-insensitive anagrams, return true. Otherwise, return false.
 
 Input Format
