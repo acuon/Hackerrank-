53cfa7009a191f6c9345df9a6b925bdbd707d404
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-10-05 10:51:34 +0530

    Create CamelCase.java

---
+++ Problem Solving/Algorithms/Strings/CamelCase.java
@@ -0,0 +1,44 @@
+//https://www.hackerrank.com/challenges/camelcase/problem?h_r=next-challenge&h_v=zen
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int camelcase(String s) {
+        int noOfWords = 1;
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i)>=65 && s.charAt(i)<=91) {
+                noOfWords++;
+            }
+        }
+        return noOfWords;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
