b689d7679abf046d4e718b3a7b682705ecb71eb3
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-10-11 16:25:27 +0530

    Create DrawingBook.java

---
+++ Problem Solving/Algorithms/Implementation/DrawingBook.java
@@ -0,0 +1,49 @@
+//https://www.hackerrank.com/challenges/drawing-book/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int pageCount(int n, int p) {
+        int fStart = 0, fLast = 0;
+        if(n%2==1) {
+            fStart = p/2;
+            fLast = (n-p)/2;
+        } else {
+            fStart = p/2;
+            fLast = (n-p+1)/2;
+        }
+        return Math.min(fStart, fLast);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
