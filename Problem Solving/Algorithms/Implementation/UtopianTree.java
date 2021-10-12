3cbb7bf20b5979bfab17e3501776a931a93a811d
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-10-12 16:21:19 +0530

    Create UtopianTree.java

---
+++ Problem Solving/Algorithms/Implementation/UtopianTree.java
@@ -0,0 +1,56 @@
+//https://www.hackerrank.com/challenges/utopian-tree/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int utopianTree(int n) {
+        int height = 1;
+        int i=1;
+        while(i<=n) {
+            if(i%2==1) {
+                height*=2;
+            } else {
+                height++;
+            }
+            System.out.println(i);
+            i++;
+        }
+        return height;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.utopianTree(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
