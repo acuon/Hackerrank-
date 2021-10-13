76fc586af58388c2d5ff8a31da0cdc182bb8e069
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-10-13 12:55:12 +0530

    Create SaveThePrisoner.java

---
+++ Problem Solving/Algorithms/Implementation/SaveThePrisoner.java
@@ -0,0 +1,61 @@
+//https://www.hackerrank.com/challenges/save-the-prisoner/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int saveThePrisoner(int n, int m, int s) {
+        return ((m-1) + (s-1))%n + 1;
+    }
+/*
+input
+2
+7 19 2
+3 7 3
+
+output
+6
+3
+*/
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int s = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = Result.saveThePrisoner(n, m, s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
