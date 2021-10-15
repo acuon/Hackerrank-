e70cb2ba1cca805946eb60ac705dfd1dba1b3b10
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-10-15 11:34:04 +0530

    Create ExtraLongFactorials.java

---
+++ Problem Solving/Algorithms/Implementation/ExtraLongFactorials.java
@@ -0,0 +1,38 @@
+//https://www.hackerrank.com/challenges/extra-long-factorials/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static void extraLongFactorials(int n) {
+        BigInteger fact = new BigInteger("1");
+        int m = n;
+        for(; m>0; m--) {
+            fact = fact.multiply(BigInteger.valueOf(m));
+        }
+        System.out.println(fact);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
