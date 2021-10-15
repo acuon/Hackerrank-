2b601f716a6d933b6608ca994fe70b0ec1b1abf8
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-10-15 11:20:03 +0530

    Create FindDigits.java

---
+++ Problem Solving/Algorithms/Implementation/FindDigits.java
@@ -0,0 +1,54 @@
+//https://www.hackerrank.com/challenges/find-digits/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int findDigits(int n) {
+        int m = n;
+        int count = 0;
+        while(m!=0) {
+            int remain = m%10;
+            if(remain!=0 && n%remain == 0) count++;
+            m/=10;
+        }
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+https://www.hackerrank.com/challenges/find-digits/problem
