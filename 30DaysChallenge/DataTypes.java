cdcb9a693524b7c57864f1c2ace2798d73361e3f
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-10-25 18:18:57 +0530

    Day 01 | Data Types

--- 30DaysChallenge/DataTypes.java
+++ 30DaysChallenge/DataTypes.java
@@ -9,6 +9,7 @@ import java.util.regex.*;
 public class Solution {
 	
     public static void main(String[] args) {
+	    
         int i = 4;
         double d = 4.0;
         String s = "HackerRank ";
