bdd37bdda2c939aeb521c60c2dd155306c85f8d0
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-10-29 12:38:51 +0530

    Create HelloWorld.java

---
+++ 30DaysChallenge/Day 00/HelloWorld.java
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/30-hello-world/problem
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); 
+		String inputString = scan.nextLine();
+		scan.close(); 
+		System.out.println("Hello, World.");
+	}
+}
