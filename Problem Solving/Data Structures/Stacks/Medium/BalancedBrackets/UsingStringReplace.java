e98d44a1301e395af93c2dc16ee3b9bace8b0795
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-10-30 12:23:29 +0530

    Create UsingStringReplace.java

---
+++ Problem Solving/Data Structures/Stacks/Medium/BalancedBrackets/UsingStringReplace.java
@@ -0,0 +1,55 @@
+//https://www.hackerrank.com/challenges/balanced-brackets/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static String isBalanced(String s) {
+        int n = s.length()/2;
+        while(n--!=0) {
+            s = s.replace("()", "");
+            s = s.replace("[]", "");
+            s = s.replace("{}", "");
+        }
+        if(s.length()!=0) {
+            return "NO";
+        }
+        return "YES";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.isBalanced(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
