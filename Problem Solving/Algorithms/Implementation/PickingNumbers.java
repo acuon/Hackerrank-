c5e72bbe6e2ffd2219b6fba2beba01897b1a946c
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-11-04 08:10:31 +0530

    Create PickingNumbers.java

---
+++ Problem Solving/Algorithms/Implementation/PickingNumbers.java
@@ -0,0 +1,50 @@
+//https://www.hackerrank.com/challenges/picking-numbers/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int pickingNumbers(List<Integer> a) {
+        int[] freq = new int[100];
+        int maxLength = 0;
+        for(int i=0; i<a.size(); i++) {
+            freq[a.get(i)]++;
+        }
+        for(int i=1; i<freq.length; i++) {
+            maxLength = Math.max(maxLength, freq[i-1]+freq[i]);
+        }
+        return maxLength;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
