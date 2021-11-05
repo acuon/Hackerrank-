5e74949047ff4e187a6d0fd8c341b3f6147a8eff
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-11-05 12:16:30 +0530

    Create LibraryFine.java

---
+++ Problem Solving/Algorithms/Implementation/LibraryFine.java
@@ -0,0 +1,64 @@
+//https://www.hackerrank.com/challenges/library-fine/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+        int fine = 0;
+        if(y1 == y2) {
+            if(m1 == m2) {
+                if(d1>d2) {
+                    fine += (d1-d2)*15;
+                }
+            } else if(m1 > m2) {
+                fine += (m1-m2)*500;
+            }
+        } else if(y1 > y2) {
+            fine = 10000;
+        }
+        return fine;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int m1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int y1 = Integer.parseInt(firstMultipleInput[2]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d2 = Integer.parseInt(secondMultipleInput[0]);
+
+        int m2 = Integer.parseInt(secondMultipleInput[1]);
+
+        int y2 = Integer.parseInt(secondMultipleInput[2]);
+
+        int result = Result.libraryFine(d1, m1, y1, d2, m2, y2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
