e8213b6476f940606d8823c73fe7faaa843dc556
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-11-21 09:02:48 +0530

    Create AlternatingCharacters.java

---
+++ Problem Solving/Algorithms/Strings/Alternating Characters/AlternatingCharacters.java
@@ -0,0 +1,55 @@
+//https://www.hackerrank.com/challenges/alternating-characters/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int alternatingCharacters(String s) {
+        int count = 0;
+        int prev = 0;
+        for(int i=1; i<s.length(); i++) {
+            if(s.charAt(i) == s.charAt(prev)) {
+                count++;
+            } else {
+                prev = i;
+            }
+        }
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.alternatingCharacters(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
