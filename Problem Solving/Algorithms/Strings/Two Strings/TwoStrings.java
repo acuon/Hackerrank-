06a03c50e6e289b297395e378db276912bc20df7
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-11-27 10:20:11 +0530

    Create TwoStrings.java

---
+++ Problem Solving/Algorithms/Strings/Two Strings/TwoStrings.java
@@ -0,0 +1,56 @@
+//https://www.hackerrank.com/challenges/two-strings/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static String twoStrings(String s1, String s2) {
+        int[] ar = new int[26];
+        for(char ch: s1.toCharArray()) {
+            ar[ch-'a']++;
+        }
+        for(char ch: s2.toCharArray()) {
+            if(ar[ch-'a']>0) return "YES";
+//             ar[ch-'a']--;
+        }
+        return "NO";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = Result.twoStrings(s1, s2);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
