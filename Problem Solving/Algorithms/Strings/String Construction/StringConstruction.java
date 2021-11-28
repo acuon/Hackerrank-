4d8db88e77f421193cec9844719bfc86094455ac
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-11-28 11:58:12 +0530

    Create StringConstruction.java

---
+++ Problem Solving/Algorithms/Strings/String Construction/StringConstruction.java
@@ -0,0 +1,50 @@
+//https://www.hackerrank.com/challenges/string-construction/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    
+    public static int stringConstruction(String s) {
+        HashSet<Character> set = new HashSet<>();
+        for(char ch: s.toCharArray()) {
+            set.add(ch);
+        }
+        return set.size();
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.stringConstruction(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
