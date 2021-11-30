9df63ff84ef253ab07df652de07903ec7440bff8
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-11-30 11:18:45 +0530

    Create GameOfThrones_1_hashmap.java

---
+++ Problem Solving/Algorithms/Strings/Game of Thrones-I/GameOfThrones_1_hashmap.java
@@ -0,0 +1,56 @@
+//https://www.hackerrank.com/challenges/game-of-thrones/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static String gameOfThrones(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch: s.toCharArray()) {
+            if(map.containsKey(ch)) {
+                map.put(ch, map.get(ch)+1);
+            } else {
+                map.put(ch, 1);
+            }
+        }
+        
+        int odd = 0;
+        for(Integer frequency : map.values()) {
+            if(frequency % 2 == 1) {
+                odd++;
+                continue;
+            }
+        }
+        
+        if(odd > 1) return "NO";
+        return "YES";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.gameOfThrones(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
