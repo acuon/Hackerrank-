bb72273b339e6b37e23ef6f15b8cc796c2ad3ebe
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-12-05 13:04:18 +0530

    Create IceCreamParlor_HashMap.java

---
+++ Problem Solving/Algorithms/Search/Ice Cream Parlor/IceCreamParlor_HashMap.java
@@ -0,0 +1,67 @@
+//https://www.hackerrank.com/challenges/icecream-parlor/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static List<Integer> icecreamParlor(int m, List<Integer> arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> ar = new ArrayList<>();
+        for(int i=0; i<arr.size(); i++) {
+            if(map.containsKey(m-arr.get(i))) {
+                ar.add(map.get(m-arr.get(i)));
+                ar.add(i+1);
+                break;
+            } else {
+                map.put(arr.get(i), i+1);
+            }
+        }
+        return ar;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                List<Integer> result = Result.icecreamParlor(m, arr);
+
+                bufferedWriter.write(
+                    result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                    + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
