f1e9bd40be74d4bf216ebd135af61807374017c3
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2021-12-08 14:27:35 +0530

    Create JumpingOnTheClouds_Revisited.java

---
+++ Problem Solving/Algorithms/Implementation/Jumping On The Clouds: Revisited/JumpingOnTheClouds_Revisited.java
@@ -0,0 +1,55 @@
+//https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int jumpingOnClouds(int[] c, int k) {
+        int n = c.length;
+        int energy = 100;
+        int i=k%n;
+        energy -= (c[i]*2 +1);
+        while(i!=0) {
+            i=(i+k)%n;
+            energy -= (c[i]*2 +1);
+        }
+        return energy;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
